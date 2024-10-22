1e8984af3ff0af0055651e10f1af3bf10424c6e4
Amit Hariyale <amithariyale125@gmail.com>
2024-10-22 12:12:52 +0530

    update

--- src/components/common/DashboardPage.jsx
+++ src/components/common/DashboardPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 // Wrapper for every page
 function DashboardPage({ children }) {
-  return <div className=" flex flex-col !h-[100%]">{children}</div>;
+  return <div className=" flex flex-col !h-[100%] p-6">{children}</div>;
 }
 
 export default DashboardPage;
